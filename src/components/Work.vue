cb179c25fb1c2d3a404c1bdc2015f8798b8340e4
developer-tamim <tawsiftamim24@gmail.com>
2024-10-23 15:38:06 +0600

    work section

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,8 +2,10 @@
     <div class="flex flex-col gap-5 py-10">
         <h1 class="text-3xl font-bold text-gray-300 mb-5">My Selected Work</h1>
         <div class="grid grid-cols-2 gap-5">
-            <div class="h-[500px] p-5 rounded-lg">1</div>
-            <div class="h-[500px] p-5 rounded-lg bg-[url(./assets/about-icon1.png) bg-center bg-cover bg-no-repeat]">2</div>
+            <div class="h-[500px] p-5 rounded-lg bg-[url(./assets/work-bg2.png) bg-center bg-cover bg-no-repeat]">
+                <img src="../assets/demo-logo.png" class="h-[90px] mx-auto" alt="" />
+            </div>
+            <div class="h-[500px] p-5 rounded-lg bg-[url(./assets/work-bg1.png) bg-center bg-cover bg-no-repeat]">2</div>
         </div>
     </div>
 </template>
