0a0a9876b131a2873bd1c7a234c4e4dc38156861
developer-tamim <tawsiftamim24@gmail.com>
2024-11-05 23:14:16 +0600

    experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,6 +1,10 @@
 <template lang="">
-    <div>
-        <h1 class="text-3xl font-bold text-gray-300 mb-5">Hear from My Clients</h1>
+    <div class="py-10 flex flex-col gap-5">
+        <h1 class="text-3xl font-bold text-gray-300 mb-5">My Work Experience</h1>
+        <div class="flex gap-5">
+            <div class="w-2/6 flex gap-5 rounded-lg p-5">1</div>
+            <div class="w-4/6 flex gap-5 rounded-lg p-5 bg-gray-900">2</div>
+        </div>
     </div>
 </template>
 <script>
