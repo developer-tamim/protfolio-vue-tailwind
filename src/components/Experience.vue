27b5b948261a266591fbcfd43619a410d69d48ad
developer-tamim <tawsiftamim24@gmail.com>
2024-11-06 22:13:32 +0600

    experiance section

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,8 +2,11 @@
     <div class="py-10 flex flex-col gap-5">
         <h1 class="text-3xl font-bold text-gray-300 mb-5">My Work Experience</h1>
         <div class="flex gap-5">
-            <div class="w-2/6 flex gap-5 rounded-lg p-5">1</div>
-            <div class="w-4/6 flex gap-5 rounded-lg p-5 bg-gray-900">2</div>
+            <div class="w-2/6 flex gap-5 rounded-lg p-5 bg-[url(./assets/computer.png)] bg-center bg-cover bg-no-repeat"></div>
+            <div class="w-4/6 flex gap-5 rounded-lg p-5 bg-gray-900">
+                <div class="flex flex-col gap-5 justify-between items-center"></div>
+                <div class="flex flex-col justify-between items-center"></div>
+            </div>
         </div>
     </div>
 </template>
