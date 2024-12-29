bbe65419f1c70f9bc06fe4b8449d425034b289e4
developer-tamim <tawsiftamim24@gmail.com>
2024-12-29 11:21:04 +0600

    experiance section

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,16 +2,33 @@
     <div class="py-10 flex flex-col gap-5">
         <h1 class="text-3xl font-bold text-gray-300 mb-5">My Work Experience</h1>
         <div class="flex gap-5">
-            <div class="w-2/6 flex gap-5 rounded-lg p-5 bg-[url(./assets/computer.png)] bg-center bg-cover bg-no-repeat"></div>
-            <div class="w-4/6 flex gap-5 rounded-lg p-5 bg-gray-900">
+            <div class="w-2/6 flex gap-5 h-[500px] rounded-lg p-5 bg-[url(./assets/computer.png)] bg-center bg-cover bg-no-repeat"></div>
+            <div class="w-4/6 flex gap-5 h-[500px] rounded-lg p-5 bg-gray-900">
                 <div class="flex flex-col w-1/5 gap-5 justify-between items-center">
                     <img src="../assets/figma.svg" class="h-[50px] bg-gray-900 p-2 rounded-xl" alt="" />
+                    <div class="p-[1px] h-28 bg-gray-800"></div>
                     <img src="../assets/framer.svg" class="h-[50px] bg-gray-900 p-2 rounded-xl" alt="" />
+                    <div class="p-[1px] h-28 bg-gray-800"></div>
                     <img src="../assets/vue.svg" class="h-[50px] bg-gray-900 p-2 rounded-xl" alt="" />
                 </div>
-                <div class="flex flex-col w-4/5 justify-between items-center"></div>
+                <div class="flex flex-col w-4/5 justify-between items-center">
+                    <div>
+                        <h1 class="text-2xl font-bold">Figma</h1>
+                        <p class="text-gray-500 text-2xl">Lead Developer - 2022 Present</p>
+                        <p class="text-gray-500 text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                    <div>
+                        <h1 class="text-2xl font-bold">Framer</h1>
+                        <p class="text-gray-500 text-2xl">Lead Developer - 2022 Present</p>
+                        <p class="text-gray-500 text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                    <div>
+                        <h1 class="text-2xl font-bold">Vue</h1>
+                        <p class="text-gray-500 text-2xl">Lead Developer - 2022 Present</p>
+                        <p class="text-gray-500 text-lg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                </div>
             </div>
-            test text
         </div>
     </div>
 </template>
